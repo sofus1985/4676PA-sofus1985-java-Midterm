e5fe35ab054d25f1b708b934f3e7582e2a11fde1
sofus1985 <egzodia75@gmail.com>
2023-06-03 22:24:33 -0500

    correction

--- src/company_app_design/CompanyEmployee.java
+++ src/company_app_design/CompanyEmployee.java
@@ -1,3 +1,83 @@
 package company_app_design;
 
 
+public class CompanyEmployee implements Employee {
+
+
+//    public String name;
+//    public String department;
+//    public int id;
+//    public int age;
+//    public int salary;
+//
+//    public  void Employee(String name, String department, int id, int age, int salary) {
+//        this.name = name;
+//        this.department = department;
+//        this.id = id;
+//        this.age = age;
+//        this.salary = salary;
+//
+//
+//    }
+//    public void setName(String name){this.name = name;}
+//    public String getName(){return name;}
+//    public void setDepartment(String department){this.department = department;}
+//    public String getDepartment(){return department;}
+//    public void setId(int id){this.id = id;}
+//    public int getId(){return id;}
+//    public void setAge(int age){this.age = age;}
+//    public int getAge(){return age;}
+//    public void setSalary(int salary){this.salary = salary;}
+//    public int getSalary(){return salary;}
+//
+//    public static void main(String[] args) {
+//        CompanyEmployee employee = new CompanyEmployee();
+//        System.out.println("name:" + employee.getName());
+//        System.out.println("department:" + employee.getDepartment());
+//        System.out.println("id:" + employee.getId());
+//        System.out.println("age;" + employee.getAge());
+//        System.out.println("salary: $" + employee.getSalary());
+//
+//    }
+
+
+
+    public static void main(String[] args) {
+
+
+
+             CompanyEmployee ex = new CompanyEmployee();
+             System.out.println(employeeName);
+             System.out.println(employeeId);
+             System.out.println(assignDepartment);
+             System.out.println(calculateSalary);
+             System.out.println(benefits);
+
+         }
+
+    @Override
+    public int employeeId() {
+        return 0;
+    }
+
+    @Override
+    public String employeeName() {
+        return null;
+    }
+
+    @Override
+    public void assignDepartment() {
+
+    }
+
+    @Override
+    public int calculateSalary() {
+        return 0;
+    }
+
+    @Override
+    public void benefits() {
+
+    }
+}
+
